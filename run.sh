a67e22f2ac12ac7bc702f749e00bcce5b95a6774
swsoyee <swsoyee@gmail.com>
2020-11-03 21:48:17 +0900

    update: requirements list and run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+pip3 install -r requirements.txt
+echo $CREDENTIALS >> credentials.json
+echo client_config:'\n  'client_id: $CLIENT_ID'\n  'client_secret: $CLIENT_SECRET >> settings.yaml
+python3 google-drive-downloader.py -i $SOURCE_FOLDER_ID
+python3 parser.py
+python3 google-drive-uploader.py -r $RESULT_FOLDER_ID
